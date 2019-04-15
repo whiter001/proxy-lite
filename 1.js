8b484757839e34ee82dda6de1f1ebe7bdd4f031e
一修 <1090562996@qq.com>
2019-04-15 21:59:40 +0800

    添加代理的账号密码验证

--- 1.js
+++ 1.js
@@ -1,5 +1,7 @@
 // 一级代理
 // 支持http/https的所有请求
+//
+// curl -x user:pwd@localhost:7777 http://baidu.com 
 const net = require('net');
 const url = require('url');
 const log = console.log.bind(console);
@@ -7,7 +9,11 @@ const local_port = process.argv[2] || 7777;
 log('default proxy port:7777, can change with first argv');
 net.createServer(c => {
     c.once('data', buf => {
-        var [isHttps, hostname, port] = parseHeaders(buf);
+        var [isHttps, hostname, port, islogin] = parseHeaders(buf);
+        if(!islogin){
+            c.end('proxy auth login failed');
+            return;
+        } 
         var s = net.connect(port, hostname, () => {
             isHttps ?
                 c.write(Buffer.from('HTTP/1.1 200 Connection established\r\nConnection: close\r\n\r\n')) :
@@ -34,9 +40,15 @@ function parseHeaders(headers) {
     path = url.parse(path);
     var hostname = path.hostname;
     var port = path.port || (isHttps ? 443 : 80);
-    /*
+    var auth = /.*Proxy-Authorization: Basic (.*)?\r\n.*/.test(headers.toString());
+    var islogin = auth && RegExp.$1 == Buffer.from('user:pwd').toString('base64');
+    /* headers:
+     * 登陆的验证
+     * Buffer.from('user:pwd').toString('base64') == 'dXNlcjpwd2Q='
+     *
       [ 'GET http://127.0.0.1:8000/a.html?a=http HTTP/1.1',
       'Host: 127.0.0.1:8000',
+      'Proxy-Authorization: Basic dXNlcjpwd2Q=',
       'User-Agent: curl/7.56.1',
       'Accept: text/plain',
       'Proxy-Connection: Keep-Alive',
@@ -44,10 +56,11 @@ function parseHeaders(headers) {
       '' ]
       [ 'CONNECT 127.0.0.1:8001 HTTP/1.1',
       'Host: 127.0.0.1:8001',
+      'Proxy-Authorization: Basic dXNlcjpwd2Q=',
       'User-Agent: curl/7.56.1',
       'Proxy-Connection: Keep-Alive',
       '',
       '' ]
       */
-    return [isHttps, hostname, port];
+    return [isHttps, hostname, port, islogin];
 }
